3d68e38d4fbabfad72405225a1807c2299c6a779
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-03 11:03:01 +0530

    Add files via upload

---
+++ input.py
@@ -0,0 +1,28 @@
+name = input("Enter your name: ")
+print("Welcome " + name)
+print("------------------------------")
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("Welcome " + name)
+print("Your Age is " + age)
+print("------------------------------")
+# Python Program for addition of two input numbers
+
+x = input("Enter First Number: ")
+y = input("Enter Second Number: ")
+
+x = int(x)
+y = int(y)
+
+res = x + y
+
+print("Sum is", res)
+print("---------------------------------")
+# Python Program for addition of two input numbers
+
+x = int(input())
+y = int(input())
+res = x + y
+print(res)
+print("------------------------------------")

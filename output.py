5ae1b1c93cb4d58d6bbd6249c1d2f9726fca7f85
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-03 11:03:50 +0530

    Add files via upload

---
+++ output.py
@@ -0,0 +1,12 @@
+print("25", "08", "2020", sep="-")
+print("Welcome", end="")
+print("to GfG")
+print("Hope you are enjoying Python", end="**")
+print("Welcome to GFG")
+a = 6
+b = 4
+c = "&"
+
+result = str(a) * a + c + str(b) * b
+print(result)
+

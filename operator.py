2a36dc8f2b1103174858a3c04ed743639a6298bb
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-03 11:03:34 +0530

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,42 @@
+# arithmetic operators
+a=25
+b=16
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a%b)
+print(a**b)
+
+print("-------------------------------")
+a=-5
+b=2
+print(a//b)
+a=-5.0
+b=2
+print(a//b)
+a=2.0
+b=-2
+print(a**b)
+a = 2
+b = 3
+
+# using the addition operator
+res = a + b
+print(res)
+print(5 + 2 * 3)
+print(5 + 3 * 4 ** 2)
+print(2**2**-1)
+print(2**(2**-1))
+print((2**2)**-1)
+
+
+# Logical Operators
+print("-------------------------------")
+a=10
+b=20
+c=30
+print(a<b and b<c)
+print(a<b or b>c)
+print(not a>b)

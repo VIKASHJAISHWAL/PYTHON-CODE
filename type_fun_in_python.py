6d8f13f8cd6cf2396b0b26d9c82c69fb3d9cef52
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-03 11:05:12 +0530

    Add files via upload

---
+++ type_fun_in_python.py
@@ -0,0 +1,28 @@
+a=10
+print(type(a))
+b=10.5
+print(type(b))
+c="Geeks"
+print(type(c))
+d=2+4j
+print(type(d))
+
+
+''''Example of boolean and none data types'''
+e=True
+print(type(e))
+f=None
+print(type(f))
+
+
+''''Example program for sequencing types'''
+a="Geeks"
+print(type(a))
+b=[10,20,30]
+print(type(b))
+c=(10,20,30)
+print(type(c))
+d={10,20,30}
+print(type(d))
+e={1:'a',2:'b',3:'c'}
+print(type(e))

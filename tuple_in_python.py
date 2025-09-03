5976bd4ebb33c7c81aba0bddcb50c66f1c0c5b07
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-03 11:04:38 +0530

    Add files via upload

---
+++ tuple_in_python.py
@@ -0,0 +1,17 @@
+t=(10,20,"geeks")
+print(t)
+t=()
+print(type(t))
+t=(10)
+print(type(t))
+t=(10,)
+print(type(t))
+
+print("------------------------------------")
+t=10,20,30,40,10
+print(t[2])
+print(t[-1])
+print(t[1:3])
+print(len(t))
+print(t.count(10))
+print(t.index(30))

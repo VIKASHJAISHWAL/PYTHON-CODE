375550eba11f690f8e2adf24ccb81b610c1c5a3b
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-04 20:38:46 +0530

    Add files via upload

---
+++ membership_operator_in_python.py
@@ -0,0 +1,11 @@
+# using string
+s="GeeksforGeeks"
+print("G"in s)
+print("Geeks" in s)
+print("gk"in s)
+# using dictionary
+print("----------------------------")
+d={10:"Ten",20:"Twenty",30:"Thirty"}
+print(10 in d)
+print(20 in d)
+print(40 in d)

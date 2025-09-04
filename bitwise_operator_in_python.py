b4c8703b0ebbd6bf6563a292bc21b7fe1fcd6ff0
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-04 20:36:14 +0530

    Add files via upload

---
+++ bitwise_operator_in_python.py
@@ -0,0 +1,25 @@
+print(bin(18))
+print(bin(12))
+print(int("0b10010",2))
+print(int("0b1100",2))
+
+# Bitwise AND operator
+print("------------------------")
+x=3
+y=6
+print(x&y)
+# Bitwise OR operator
+print("-------------------------")
+print(x|y)
+# Bitwise NOT operator
+print("-------------------------")
+print(-x)
+# Bitwise XOR operator
+print("----------------------------")
+print(x^y)
+# Bitwise Left Shift Operator
+print("---------------------------")
+print(x<<2)
+# Bitwise Right Shift Operator
+print("----------------------------")
+print(x>>2)

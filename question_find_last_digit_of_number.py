3e52cd85239d3e8543bc6c84829ee09593329c1b
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-04 20:40:39 +0530

    Add files via upload

---
+++ question_find_last_digit_of_number.py
@@ -0,0 +1,7 @@
+x=int(input("Enter  a number:"))
+last_digit=x%10
+print("The last digit of the number is:",last_digit)
+print("-------------------------------------")
+x=int(input("Enter  a number:"))
+last_digit=abs(x)%10
+print("The last digit of the number is:",last_digit)

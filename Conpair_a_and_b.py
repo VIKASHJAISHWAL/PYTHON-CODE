c8204082727aba109184091388fa3d95c91b4970
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-05 17:58:04 +0530

    Add files via upload

---
+++ Conpair_a_and_b.py
@@ -0,0 +1,8 @@
+a=int(input("Enter a values of a:"))
+b=int(input("Enter a values of b:"))
+if a>b:
+    print(a,"is greater than b")
+elif a<b:
+    print(a,"is less than b")
+else:
+    print("Both numbers are equal")

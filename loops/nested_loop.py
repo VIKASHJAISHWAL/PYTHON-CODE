57bd37762c2ce7ed5926b0bbdcfca78b4fef2f1b
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-08 00:35:57 +0530

    Add files via upload

---
+++ loops/nested_loop.py
@@ -0,0 +1,15 @@
+for i in range(1,1*10+1,1):
+    print(i,end=" ")
+print()    
+for i in range(2,2*10+1,1): 
+    print(i,end=" ")
+print()      
+for i in range(3,3*10+1,1):
+    print(i,end=" ")
+print()    
+print("-----------------------")
+for i in range(1,11):
+    for j in range(i,i*10+1,i):
+        print(j,end=" ")
+    print()    
+        

4c44d5e2e54099769b98f41e167d5925faa846a9
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-08 00:37:41 +0530

    Add files via upload

---
+++ loops/range.py
@@ -0,0 +1,21 @@
+r=range(5)
+l=list(r)
+print(l)
+
+print("-----------------------------")
+r=range(5)
+print(r)
+l=list(r)
+print(type(l))
+
+print("-------------------------------")
+r=range(-2,2)
+l=list(r)
+print(l)
+
+print("--------------------------------")
+r=range(10,20,2)
+l=list(r)
+print(l)
+# --------------------------------
+# [10, 12, 14, 16, 18] output

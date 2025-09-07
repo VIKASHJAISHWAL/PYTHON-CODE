16f4045df25eaa4500ffb7c4532f60471b338056
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-08 00:38:06 +0530

    Add files via upload

---
+++ loops/table.py
@@ -0,0 +1,10 @@
+n=int(input("Enter a number:"))
+for i in range(1,11):
+    print(n,"x",i,"=",n*i)
+    i=i+1
+print("----------------------")
+n=int(input("Enter the value of n:"))
+m=int(input("Enter the value of m:"))
+for i in range(1,m):
+    print(i*n)
+    i=i+1    

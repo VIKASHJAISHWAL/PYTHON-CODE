9ac15926ecd2edcd06a232f00acdd0efabfd216d
VIKASHJAISHWAL <144373433+VIKASHJAISHWAL@users.noreply.github.com>
2025-09-08 00:34:38 +0530

    Add files via upload

---
+++ loops/for_loop.py
@@ -0,0 +1,24 @@
+l=[10,20,30,40,50]
+for x in l:
+    print(x)
+print("---------------------------")
+s="vikash jaishwal"
+for x in s:
+    print(x)  
+print("---------------------------")
+s="vikash jaishwal"
+for x in s:
+    print(x) 
+    print(x)  
+
+print("--------------------------------")
+for x in range(5):
+    print(x)     
+print("--------------------------------")
+for x in range(20):
+    if x%6==0:
+        print(x) 
+print("----------------------------------")
+l=[10,20,30,40,50] 
+for i in range(len(l)):
+    print(l[i])               
